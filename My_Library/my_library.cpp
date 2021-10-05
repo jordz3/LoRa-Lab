72ed49de9041047a2118be483c42b8f36fdc1f54
jordz3 <jordz_3@hotmail.com>
2021-10-05 15:15:24 +0800

    Update my_library.cpp

--- My_Library/my_library.cpp
+++ My_Library/my_library.cpp
@@ -1,17 +1,25 @@
 #include "my_library.h"
 
-String logPrint; //
-int NUM_PARAMETERS = 4; // number of unique parameters passed
+String logPrint; // variable to record logging data
+int NUM_PARAMETERS = 4; // number of unique parameters passed 
 int NUM_PACKETS = 20; // number of packets to send
 int dly = 2000; // delay for transmission rate (ms)
 
-// Storing parameters to test
+/** 
+ * Storing of parameters to cycle through
+ * NOTE
+ * Spreading Factor must remain the same across the experiment and cannot be cycled.
+ */
+
 long int bandwidth[] = {500E3, 500E3, 500E3, 500E3};
 int codingRate[] = {5, 5, 5, 5};
 long int frequency[] = {915E6, 915E6, 915E6, 915E6};
 int spreadingFactor[] = {7, 7, 7, 7};
 int txPower[] = {20, 20, 20, 20};
 
+/**
+ * Function to set parameters, taking an integer to loop through
+ */
 String setParameters(int i)
 {
 
@@ -27,7 +35,7 @@ String setParameters(int i)
   int sf = spreadingFactor[i];
   int tx = txPower[i];
 
-  // Storing in csv to print to log file
+  // Storing in csv format to print to log file via logPrint
   logPrint = "";
   logPrint = logPrint +
              bw + "," +
