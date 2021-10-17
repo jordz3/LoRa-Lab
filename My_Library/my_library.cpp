b06d96485669b6efb2a667e3659fc0cb5d42cf04
jordz3 <jordz_3@hotmail.com>
2021-10-17 17:11:17 +0800

    Update my_library.cpp

--- My_Library/my_library.cpp
+++ My_Library/my_library.cpp
@@ -14,7 +14,7 @@ int dly = 1000; // delay for transmission rate (ms)
 
 // Storing parameters to test
 //Supported values are 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3, 41.7E3, 62.5E3, 125E3, 250E3, and 500E3.
-long int bandwidth[] = {500E3, 500E3, 500E3, 500E3, 500E3, 500E3, 250E3, 250E3, 250E3}; 
+long int bandwidth[] = {250E3, 250E3, 250E3}; 
 
 //Supported values are between 5 and 8, these correspond to coding rates of 4/5 and 4/8. The coding rate numerator is fixed at 4.
 int codingRate[] = {5, 5, 5, 5, 5, 5, 5, 5, 5};
@@ -26,7 +26,7 @@ long int frequency[] = {915E6, 915E6, 915E6, 915E6, 915E6, 915E6, 915E6, 915E6, 
 int spreadingFactor[] = {7, 7, 7, 7, 7, 7, 7, 7, 7};
 
 //Supported values are 2 to 20 for PA_OUTPUT_PA_BOOST_PIN, and 0 to 14 for PA_OUTPUT_RFO_PIN.
-int txPower[] = {2, 4, 6, 8, 10, 12, 14, 10, 12, 14};
+int txPower[] = {10, 12, 14, 8, 10, 12, 14, 10, 12, 14};
 
 //Minimize bandwidth and maximize spreading factor to boost link budget. Maximize coding rate to boost reliability.
 
